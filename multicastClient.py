caeb6c9d078ba8004d451663464674691fc59f98
mtnoreika <mn55@st-andrews.ac.uk>
2017-02-15 20:36:45 +0000

    Implemented exchange of control packets using TCP

--- multicastClient.py
+++ multicastClient.py
@@ -4,129 +4,51 @@ import socket
 import struct
 import sys
 import protocol
+import select
 
 from packet import *
+from packet_parser import *
 
-
-# Creata a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Bind the socket to the server address
-sock.bind(protocol.server_address)
-
+# Set up udp socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind(protocol.server_address)
 group = socket.inet_aton(protocol.multicast_ip)
 mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-
+# Set up tcp socket
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+tcp_socket.bind(("", protocol.tcp_port))
+tcp_socket.listen(1)
 
 packet_number = 0
 packets_missed = []
+sockets = [udp_socket]
 
-def acknowledgeReceipt():
-	sock.sendto('OK', address)
-	print >> sys.stderr, 'Acknowledge response sent.'
+print >> sys.stderr, 'Waiting for data...\n'
 
-	print >> sys.stderr, '\n# File is being transmitted #\n'
-
-
-def parsePacket(data, address):
-	global packet_number
-	global packets_missed
-
-	if (data[4:5] == protocol.start_packet_type):
-		print >> sys.stderr, '\n# Start of transmission packet received #'
-
-		packet_number = 0
-
-		packets_missed = []
-
-		readStreamStartPacket(data, address)
-
-	if (data[4:5] == protocol.data_packet_type):
-		packet = DataPacket.unpackHeader(data[:10]);
-
-		packet_number += 1
-
-		if (packet[3] in packets_missed):
-			packets_missed.remove(packet[3])
-
-		elif (packet_number != packet[3]):
-			packets_missed.append(packet_number)
-
-			packet_number += 1
-
-
-		readDataPacket(packet, data)
-
-	if (data[4:5] == protocol.end_packet_type):
-		readStreamEndPacket(data)
-
-
-def readStreamStartPacket(data, address):
-	global tcp_socket
-	global file
-	
-	packet = StreamStartPacket.unpack(data)
-
-	print >> sys.stderr, packet
-
-	file_name = packet[5].split(b'\0',1)[0]
-	print >> sys.stderr, 'File name: %s' % file_name
-
-	file = open(file_name + ".received", "w+b")
-
-	print >> sys.stderr, address[0]
-
-	tcp_socket.connect((address[0], protocol.tcp_port))
-
-	tcp_socket.send("OK")
-
-
-def readStreamEndPacket(data):
-	packet = StreamEndPacket.unpack(data)
-
-	print >> sys.stderr, "# End of stream packet received #"
-	print >> sys.stderr, packet
-
-	global packet_number
-	print >> sys.stderr, "Packets received: %d\n" % (packet_number - len(packets_missed))
-
-	if (len(packets_missed) != 0):
-		# Check if packets are missing and request the mising ones 
-		print >> sys.stderr, "Packets missed: %d \n %s" % (len(packets_missed), packets_missed)
-
-		requestPacket = RequestPacket.packHeader(len(packets_missed)) + RequestPacket.packPayload(len(packets_missed) , packets_missed)
-
-		print >> sys.stderr, "Sending request packet: %d bytes" % len(requestPacket)
-
-	 	sock.sendto(requestPacket, address)
-	else:
-		success_packet = SuccessPacket.pack()
-
-		print >> sys.stderr, "Sending success packet: %d bytes" % len (success_packet)
-
-		sock.sendto(success_packet, address)
- 
-def readDataPacket(packet, data):
-	file.seek(118 * (packet[3] - 1))
-
-	file.write(data[10:(10 + packet[4])])
-
-print >> sys.stderr, 'Waiting for data...'
 
 # Receive the data and respond to the sender
 while True:
-	data, address = sock.recvfrom(1024)
-
-	# print >> sys.stderr, 'Received %s bytes from %s' % (len(data), address)
-
-	if (data[:3] == protocol.name):
-		parsePacket(data, address)
-	else:
-		print >> sys.stderr, 'Invalid protocol. Packet dropped.'
+	if len(sockets) == 1:
+		tcp_conn, client_address = tcp_socket.accept()
+		sockets.append(tcp_conn)
+		print >> sys.stderr, "Connection with sender established:", client_address
 
 
+	input_ready, output_ready, _ = select.select(sockets, [], [])
+
+	for socket in input_ready:
+		try:
+			data, address = socket.recvfrom(protocol.max_packet_size)
+
+			if (data[:3] == protocol.name):
+				parsePacket(data, tcp_conn)
+			else:
+				print >> sys.stderr, 'Invalid protocol. Packet dropped.'
+		except:
+			sockets.remove(tcp_conn)
+
+          
+			
--- multicastServer.py
+++ multicastServer.py
@@ -9,48 +9,137 @@ import select
 
 from packet import *
 
-
-number_of_clients = 2;
-
-# Sends the packet using multicast to multiple recipients
-def sendPacket(packet):
-	send = sock.sendto(packet, protocol.multicast_group)
-
-	
-# Create a datagram socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Set timeout for the socket
-sock.settimeout(protocol.socket_timeout)
-
-# Set the time-to-live for messages
-ttl = struct.pack('b', protocol.time_to_live)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+number_of_clients = 2
+sockets = []
+receivers = ["pc3-010-l.cs.st-andrews.ac.uk", "pc3-009-l.cs.st-andrews.ac.uk"]
 
 start_time = time.clock()
 
+# Sends the packet using multicast to multiple recipients
+def sendPacket(packet):
+	send = udp_sock.sendto(packet, protocol.multicast_group)
+
+start_time = time.clock()
+
+# Create a datagram socket
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set timeout for the socket
+udp_sock.settimeout(protocol.socket_timeout)
+
+# Set the time-to-live for messages
+ttl = struct.pack('b', protocol.time_to_live)
+udp_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+
 startPacket = StreamStartPacket("movie.mjpeg", 1024, 4171)
 
-print >> sys.stderr, 'Start of stream packet sent.'
+# Establishing connections with the receivers
+for receiver in receivers:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((receiver, protocol.tcp_port))
+    sockets.append(sock)
+    print >> sys.stderr, "Connected to receiver: %s" % receiver
+    sock.send(startPacket.pack())
 
-sendPacket(startPacket.pack())
-
-
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcp_socket.bind(("", protocol.tcp_port))
-tcp_socket.listen(True)
-
-
-conn, addr = tcp_socket.accept()
-print 'Connection from address:', addr
+clients_listening = 0
+file_sent = False
 
 while True:
+	input_ready, output_ready, _ = select.select(sockets, [], [])
+
+	for socket in input_ready:
+		data, address = socket.recvfrom(protocol.max_packet_size)
+
+		print >> sys.stderr, "Received: %d bytes" % len(data)
+
+		if data[4:5] == protocol.success_packet_type:
+
+			clients_listening += 1;
+
+			if (number_of_clients == clients_listening):
+				with open("movie.mjpeg", "rb") as file:
+					byte = file.read(1)
+					payload = bytearray()
+
+					packet_number = 0
+
+					while byte != "":
+						payload.append(byte)
+
+						if len(payload) == 118:
+							
+							packet_number += 1
+
+							dataPacketHeader = DataPacket.packHeader(packet_number, 118)
+
+							dataPacket = dataPacketHeader + payload
+
+							sendPacket(dataPacket)
+							payload = bytearray()	
+						
+						byte = file.read(1)
+
+					packet_number += 1
+
+					dataPacketHeader = DataPacket.packHeader(packet_number, len(payload))
+
+					dataPacket = dataPacketHeader + payload;
+
+					sendPacket(dataPacket)
+
+					print >> sys.stderr, "Packets sent %d" % packet_number
+
+					#Sending end of stream packet		
+					endPacket = StreamEndPacket.pack()
+
+					sendPacket(endPacket);
+
+					end_time = time.clock()
+			
+					print >> sys.stderr, "Time taken: %0.5f seconds" % (end_time - start_time)
+
+		elif data[4:5] == protocol.request_packet_type:
+			request_packet_header = RequestPacket.unpackHeader(data[:8])
+			request_packet_payload = RequestPacket.unpackPayload(request_packet_header[3], data[8:])
+
+			print >> sys.stderr, request_packet_header
+
+			with open("movie.mjpeg", "rb") as file:
+				for missed_packet in request_packet_payload:
+					file.seek((missed_packet - 1) * 118)
+					dataPayload = file.read(128)
+					dataPacketHeader = DataPacket.packHeader(missed_packet, len(dataPayload))
+					dataPacket = dataPacketHeader + dataPayload
+					sendPacket(dataPacket)
+
+			#Sending end of stream packet		
+			endPacket = StreamEndPacket.pack()
+
+			sendPacket(endPacket);		
+
+					
+
+# print >> sys.stderr, 'Start of stream packet sent.'
+
+# sendPacket(startPacket.pack())
+
+
+# tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# tcp_socket.bind(("", protocol.tcp_port))
+# tcp_socket.listen(True)
+
+
+# conn, addr = tcp_socket.accept()
+# print 'Connection from address:', addr
+
+# while True:
    
-    ready_socks,_,_ = select.select(socks, [], []) 
-    for sock in ready_socks:
-        data, addr = sock.recvfrom(1024) # This is will not block
-        print "Received ack:", data
+#     ready_socks,_,_ = select.select(socks, [], []) 
+#     for sock in ready_socks:
+#         data, addr = sock.recvfrom(1024) # This is will not block
+#         print "Received ack:", data
 
 # while True:
 # 	try:
